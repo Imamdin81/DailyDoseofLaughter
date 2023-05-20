91979522de5adc9a63573efe64cf5bf8d082d461
Imam Din <41516850+Imamdin81@users.noreply.github.com>
2023-05-20 16:56:36 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://comicsconnoisseurs.com/graphql` + encodeURI(path as string)
+					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
 				}`,
 			},
 		};
